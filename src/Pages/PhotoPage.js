22f965c3802120be557b3d2fdab004c197be5bce
Mark Dearoz <markdearoz08@gmail.com>
2021-09-07 16:22:47 -0400

    removed useEffect

--- src/Pages/PhotoPage.js
+++ src/Pages/PhotoPage.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from 'react'
+import { useCallback, useRef, useState } from 'react'
 import { motion, AnimatePresence, AnimateSharedLayout } from 'framer-motion'
 import { Link } from 'react-router-dom'
 import Braille from '../Components/Braille'
